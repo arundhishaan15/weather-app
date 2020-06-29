d7c79ec07026f5b1c4b288c64bc574f60f47f6f4
Rohithshan <53473402+Rohithshan@users.noreply.github.com>
2020-06-29 20:22:27 +0530

    changed few html and javascipt

--- script.js
+++ script.js
@@ -3,7 +3,16 @@
  */
 
 // API_KEY for maps api
+window.onload = function() {
+
+  document.getElementById('city-input').focus();
+  document.getElementById('city-input').select();
+
+}
+
 let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
+
+document.addEventListener('keypress', doc_keypress);
 
 /**
  * Retrieve weather data from openweathermap
@@ -11,17 +20,30 @@ let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
 getWeatherData = (city) => {
   const URL = "https://api.openweathermap.org/data/2.5/weather";
   const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`;
+  console.log(FULL_URL);
   const weatherPromise = fetch(FULL_URL);
   return weatherPromise.then((response) => {
     return response.json();
   });
 };
 
+
+// if enter is pressed then the function should work 
+function doc_keypress(event) {
+  if(event.keyCode === 13) {
+    searchCity();  
+  }
+}
+
 /**
  * Retrieve city input and get the weather data
  */
 searchCity = () => {
   const city = document.getElementById("city-input").value;
+  removeSearch();
+  document.getElementById('city-input').focus();
+  document.getElementById('city-input').select();
+  
   getWeatherData(city)
     .then((res) => {
       showWeatherData(res);
@@ -29,19 +51,32 @@ searchCity = () => {
     .catch((error) => {
       console.log(error);
       console.log("Something happened");
-    });
-};
+    })
+
+}
 
 /**
  * Show the weather data in HTML
  */
 showWeatherData = (weatherData) => {
+  //CODE GOES HERE
+  
   console.log(weatherData);
-  document.getElementById("city-name").innerText = weatherData.name;
-  document.getElementById("weather-type").innerText =
-    weatherData.weather[0].main;
-  document.getElementById("temp").innerText = weatherData.main.temp;
-  document.getElementById("min-temp").innerText = weatherData.main.temp_min;
-  document.getElementById("max-temp").innerText = weatherData.main.temp_max;
+  document.getElementById('city-name').innerText = weatherData.name;
+  document.getElementById('weather-type').innerText = weatherData.weather[0].main;
+  document.getElementById('temp').innerText = weatherData.main.temp;
+  document.getElementById('min-temp').innerText = weatherData.main.temp_min;
+  document.getElementById('max-temp').innerText = weatherData.main.temp_max;
   document.getElementById("weather-output").classList.add("visible");
-};
+  document.getElementById("img-container").src = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
+
+}
+
+function removeSearch() {
+  console.log('working');
+  let emptcode = `<input id="city-input" class="form-control form-control-lg" type="text" placeholder="Search city">`;
+  document.getElementById('city-input-div').innerHTML = emptcode; 
+}
+
+ 
+
