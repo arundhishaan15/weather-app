c7d45c72e3dcfb82c76e582e1d6ed65cb3dc996a
Rohithshan <53473402+Rohithshan@users.noreply.github.com>
2020-06-30 09:26:21 +0530

    css and javascript changes

--- script.js
+++ script.js
@@ -20,7 +20,6 @@ document.addEventListener('keypress', doc_keypress);
 getWeatherData = (city) => {
   const URL = "https://api.openweathermap.org/data/2.5/weather";
   const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`;
-  console.log(FULL_URL);
   const weatherPromise = fetch(FULL_URL);
   return weatherPromise.then((response) => {
     return response.json();
@@ -45,12 +44,16 @@ searchCity = () => {
   document.getElementById('city-input').select();
   
   getWeatherData(city)
-    .then((res) => {
-      showWeatherData(res);
+    .then((response) => {
+      showWeatherData(response);
     })
     .catch((error) => {
       console.log(error);
       console.log("Something happened");
+      removeSearch();
+      document.getElementById('city-input').focus();
+      document.getElementById('city-input').select();
+      
     })
 
 }
@@ -60,15 +63,26 @@ searchCity = () => {
  */
 showWeatherData = (weatherData) => {
   //CODE GOES HERE
-  
   console.log(weatherData);
-  document.getElementById('city-name').innerText = weatherData.name;
-  document.getElementById('weather-type').innerText = weatherData.weather[0].main;
-  document.getElementById('temp').innerText = weatherData.main.temp;
-  document.getElementById('min-temp').innerText = weatherData.main.temp_min;
-  document.getElementById('max-temp').innerText = weatherData.main.temp_max;
-  document.getElementById("weather-output").classList.add("visible");
-  document.getElementById("img-container").src = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
+  if(weatherData.name != undefined){
+    document.getElementById('city-name').innerText = weatherData.name;
+    document.getElementById('weather-type').innerText = weatherData.weather[0].main;
+    document.getElementById('temp').innerText = weatherData.main.temp;
+    document.getElementById('min-temp').innerText = weatherData.main.temp_min;
+    document.getElementById('max-temp').innerText = weatherData.main.temp_max;
+    document.getElementById("weather-output").classList.add("visible");
+    document.getElementById("img-container").src = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
+  } else {
+    document.getElementById('city-name').innerText = weatherData.name;
+    document.getElementById('weather-type').innerText = '--';
+    document.getElementById('temp').innerText = '--';
+    document.getElementById('min-temp').innerText = '--';
+    document.getElementById('max-temp').innerText = '--';
+    document.getElementById("weather-output").classList.add("visible");
+    document.getElementById("img-container").src = '--';
+  }
+
+  
 
 }
 
